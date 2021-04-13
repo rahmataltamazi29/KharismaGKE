9aaeb07adb15d974b2a3594e22a2aac7d067be31
rahmataltamazi29 <rahmataltamazi_19@kharisma.ac.id>
2021-04-13 14:23:01 -0700

    add

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Nama : rahmat Nim : 51018018 Jurusan : SI\n')
 })
 
 app.listen(PORT, HOST)
